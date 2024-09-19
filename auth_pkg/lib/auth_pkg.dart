9a6418e5641a6b34dd6fde86c1218d3941e3158f
Owen <jeffowen04@gmail.com>
2024-09-19 13:24:21 +0300

    Profile Page UI

--- auth_pkg/lib/auth_pkg.dart
+++ auth_pkg/lib/auth_pkg.dart
@@ -1,6 +1,10 @@
 library auth_pkg;
 
+// widgets
 export 'src/widgets/button.dart';
+export 'src/widgets/input_field.dart';
+
+// pages
 export 'src/pages/login.dart';
-// export 'src/assets/images/genesis_house_img.dart';
+export 'src/pages/signup.dart';
 export 'src/pages/genesis.dart';
